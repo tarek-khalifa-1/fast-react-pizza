e0fe893681bea37bf17c1c12296cc918baefd2fe
tarek-khalifa-1 <user.tarek.dev@gmail.com>
2026-04-02 14:52:32 +0200

    finished fetch menu data by react router loader

--- src/ui/AppLayout.jsx
+++ src/ui/AppLayout.jsx
@@ -1,10 +1,15 @@
-import { Outlet } from "react-router-dom";
+import { Outlet, useNavigation } from "react-router-dom";
 import Header from "./Header";
 import CartOverview from "../features/cart/CartOverview";
+import LoaderSpinner from "./LoaderSpinner";
 
 function AppLayout() {
+  const navigation = useNavigation();
+  const isLoading = navigation.state === "loading";
+
   return (
-    <div>
+    <div className="layout">
+      {isLoading && <LoaderSpinner />}
       <Header />
       <main>
         <Outlet />
