389f33f8466cb203f6f2d606cb6ce745c59fa053
tarek-khalifa-1 <user.tarek.dev@gmail.com>
2026-04-02 13:26:57 +0200

    finished app layout and page routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,8 @@
+import { RouterProvider } from "react-router-dom";
+import router from "./routes";
+
 function App() {
-  return <div>Hello Vite</div>;
+  return <RouterProvider router={router}></RouterProvider>;
 }
 
 export default App;
